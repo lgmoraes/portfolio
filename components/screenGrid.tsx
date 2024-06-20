f1184ed60d1481ee2ce994921349ad0233ce1af7
Louis Moraes <lmoraes@hotmail.fr>
2024-06-20 23:01:50 +0200

    fix(ScreenGrid): Eliminate CLS by moving layout logic to CSS

--- components/screenGrid.tsx
+++ components/screenGrid.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { CSSProperties, useEffect, useState } from 'react';
-
-const IMAGE_RATIO = 3 / 4;
-const LAYOUT_BREAKPOINT = 1.7;
+import { CSSProperties } from 'react';
+import './screenGrid.css';
 
 const styles: { background: CSSProperties } = {
   background: {
@@ -14,39 +12,11 @@ const styles: { background: CSSProperties } = {
 };
 
 function ScreenGrid({ data }: { data: Portfolio }) {
-  const [availableWidth, setAvailableWidth] = useState<number>(0);
-  const [availableHeight, setAvailableHeight] = useState<number>(0);
-
-  let imageWidth = '';
-  let imageHeight = '';
-
-  if (availableWidth / availableHeight > LAYOUT_BREAKPOINT) {
-    /* Horizontal layout */
-    imageWidth = 'w-1/4';
-    imageHeight = `h-1/2`;
-  } else {
-    /* Vertical layout */
-    imageWidth = 'w-1/2';
-    imageHeight = (availableWidth / 2) * IMAGE_RATIO + 'px';
-  }
-
-  useEffect(() => {
-    const handleResize = () => {
-      setAvailableWidth(window.innerWidth);
-      setAvailableHeight(window.innerHeight);
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
-    <div className="flex h-screen w-full flex-wrap justify-center overflow-hidden p-0">
+    <div className="screenGrid flex h-screen w-full flex-wrap justify-center overflow-hidden p-0">
       {data.projects.map((p) => (
         <a
-          className={`${imageWidth} ${imageHeight} relative shrink-0 overflow-hidden`}
+          className={`w-1/2 relative shrink-0 overflow-hidden`}
           key={p.id}
           href={p.link}
         >
