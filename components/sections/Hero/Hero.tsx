dd6daf4816b1d7f914a4d7f6f71ee3bbdedb31ba
Louis Moraes <lmoraes@hotmail.fr>
2024-12-09 22:48:10 +0100

    feat(Hero): Add responsiveness, styling iteration 3

--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -12,20 +12,20 @@ export const Hero = ({}: {}) => {
       <div className=""></div>
 
       <div className="space-y-12">
-        <div className="flex flex-col gap-1 bg-black/30 p-9 leading-none backdrop-blur-sm text-shadow-lg lg:w-fit lg:px-28">
-          <p className="text-7xl tracking-wide">
+        <div className="flex flex-col gap-1 bg-black/30 p-7 px-0 leading-none backdrop-blur-sm text-shadow-lg sm:p-9 lg:w-fit lg:px-28">
+          <p className="text-4xl tracking-wide sm:text-6xl lg:text-7xl">
             {process.env.NEXT_PUBLIC_SITE_OWNER}
           </p>
-          <hr className="m-auto my-4 w-2/4 border" />
+          <hr className="m-auto my-4 w-44 border sm:w-60 lg:w-1/2" />
           <div className="space-y-1">
             <h2
-              className="text-4xl font-medium"
+              className="text-lg font-medium sm:text-2xl lg:text-4xl"
               style={{ letterSpacing: '0.37em' }}
             >
               Developpeur Web
             </h2>
             <h3
-              className="text-4xl font-medium"
+              className="text-lg font-medium sm:text-2xl lg:text-4xl"
               style={{ letterSpacing: '0.37em' }}
             >
               Next / Shopify
@@ -34,7 +34,7 @@ export const Hero = ({}: {}) => {
         </div>
         <Button
           variant="white"
-          className="ml-24 flex h-12 w-80 rounded-none text-xl font-bold uppercase tracking-wider shadow"
+          className="mx-auto flex h-12 w-64 rounded-none text-base font-bold uppercase tracking-wider shadow sm:text-lg lg:ml-24 lg:w-80 lg:text-xl"
           asChild
         >
           <Link href="#contact">Contactez-moi</Link>
