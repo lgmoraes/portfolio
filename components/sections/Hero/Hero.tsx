e0f541a4c3b594a735aad80276bdc4ab5025d639
Louis Moraes <lmoraes@hotmail.fr>
2024-12-09 18:59:46 +0100

    feat(Hero): Styling iteration 2

--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export const Hero = ({}: {}) => {
   return (
-    <section className="h-content flex flex-col justify-between text-center font-title uppercase text-slate-50">
+    <section className="flex h-content flex-col justify-between text-center font-title uppercase text-slate-50">
       <h1 className="sr-only">
         Louis Moraes - Création de sites modernes et performants avec Next.js et
         Shopify
@@ -12,19 +12,21 @@ export const Hero = ({}: {}) => {
       <div className=""></div>
 
       <div className="space-y-12">
-        <div className="flex max-w-3xl flex-col gap-1 bg-black/20 p-9 leading-none backdrop-blur-sm text-shadow-lg">
-          <p className="text-7xl">{process.env.NEXT_PUBLIC_SITE_OWNER}</p>
+        <div className="flex flex-col gap-1 bg-black/30 p-9 leading-none backdrop-blur-sm text-shadow-lg lg:w-fit lg:px-28">
+          <p className="text-7xl tracking-wide">
+            {process.env.NEXT_PUBLIC_SITE_OWNER}
+          </p>
           <hr className="m-auto my-4 w-2/4 border" />
           <div className="space-y-1">
             <h2
               className="text-4xl font-medium"
-              style={{ letterSpacing: '0.33em' }}
+              style={{ letterSpacing: '0.37em' }}
             >
               Developpeur Web
             </h2>
             <h3
               className="text-4xl font-medium"
-              style={{ letterSpacing: '0.33em' }}
+              style={{ letterSpacing: '0.37em' }}
             >
               Next / Shopify
             </h3>
@@ -32,7 +34,7 @@ export const Hero = ({}: {}) => {
         </div>
         <Button
           variant="white"
-          className="ml-24 flex h-12 w-80 rounded-none text-xl font-bold uppercase shadow"
+          className="ml-24 flex h-12 w-80 rounded-none text-xl font-bold uppercase tracking-wider shadow"
           asChild
         >
           <Link href="#contact">Contactez-moi</Link>
