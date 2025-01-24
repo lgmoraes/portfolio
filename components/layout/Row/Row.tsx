2be1dbadd2205f9c93a3c03a8074bd48c350e92b
Louis Moraes <lmoraes@hotmail.fr>
2025-01-24 14:31:12 +0100

    feat(Row): Add padding prop

--- components/layout/Row/Row.tsx
+++ components/layout/Row/Row.tsx
@@ -3,14 +3,26 @@ import { cn } from '@/lib/utils';
 type RowProps = {
   children: React.ReactNode;
   fullPage?: boolean;
+  padding?: 'none' | 'sm' | 'md' | 'lg' | 'xl';
   video?: string;
   className?: string;
   id?: string;
 };
 
+type PaddingSizeKey = 'none' | 'sm' | 'md' | 'lg' | 'xl';
+
+const paddingSize: Record<PaddingSizeKey, string> = {
+  none: '',
+  sm: 'px-2 sm:px-3 lg:px-4',
+  md: 'px-4 sm:px-6 lg:px-8',
+  lg: 'px-5 sm:px-8 lg:px-10',
+  xl: 'px-6 sm:px-9 lg:px-12',
+};
+
 export const Row = ({
   children,
   fullPage = false,
+  padding = 'none',
   video,
   className = '',
   id,
@@ -19,8 +31,9 @@ export const Row = ({
     <div
       id={id}
       className={cn(
-        'relative mx-auto w-full px-4 sm:px-6 lg:px-8',
+        'relative mx-auto w-full',
         fullPage && 'min-h-content',
+        padding && paddingSize[padding],
         className,
       )}
     >
