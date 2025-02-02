7b9c9d97d3fd8deb7d885867ca0c8ae09b99049f
Louis Moraes <lmoraes@hotmail.fr>
2025-02-02 09:37:28 +0100

    feat(sentry): Integrate Sentry for error tracking and monitoring

---
+++ sentry.edge.config.ts
@@ -0,0 +1,34 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  sendDefaultPii: false,
+
+  beforeSend(event) {
+    // Supprimer toutes les données utilisateur
+    if (event.user) {
+      event.user = undefined;
+    }
+    // Supprimer les adresses IP
+    if (event.request && event.request.headers) {
+      delete event.request.headers['X-Forwarded-For'];
+      delete event.request.headers['REMOTE_ADDR'];
+    }
+    if (event.contexts && event.contexts.device) {
+      delete event.contexts.device.ip_address;
+    }
+    return event;
+  },
+});
